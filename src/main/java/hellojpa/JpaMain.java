c2617e4490c10eb9ff15d0dee87014a1d435950a
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-28 09:50:44 +0900

    준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,10 +21,13 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            em.flush();
+//            em.detach(member);
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("==========================");
             tx.commit();
