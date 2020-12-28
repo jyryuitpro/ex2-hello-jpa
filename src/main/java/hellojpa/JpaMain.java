90b78e94b1c136a01e24107f469535b978a4218c
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-28 09:37:18 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,35 +21,12 @@ public class JpaMain {
 
         try {
 
-            //비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            //영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
-
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-
-//            System.out.println("result = " + (findMember1 == findMember2));
-
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
-
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-
-//            em.persist(member1);
-//            em.persist(member2);
-
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            em.flush();
 
             System.out.println("==========================");
-
             tx.commit();
         } catch (Exception e){
             tx.rollback();
